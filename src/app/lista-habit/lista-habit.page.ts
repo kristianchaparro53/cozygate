3d2660e3eb1517378d3141d80e228d2ec59bbc6f
perlis2433 <perlis2433@gmail.com>
2023-07-09 22:54:00 -0600

    tab2 ajustable

--- src/app/lista-habit/lista-habit.page.ts
+++ src/app/lista-habit/lista-habit.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lista-habit',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaHabitPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  Agregar(){
+    this.router.navigate(['/lista-hab'])
+  }
 }
